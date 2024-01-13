f9a0aba2ca0a87d7df68c1ae03752d393d193ef9
Dolliwyx <35372554+Dolliwyx@users.noreply.github.com>
2024-01-13 18:14:11 +0800

    refactor: add type safety

--- structures/Database.py
+++ structures/Database.py
@@ -1,4 +1,5 @@
 from supabase import create_client, Client
+from common.types import *
 
 class Database:
     def __init__(self, url: str, key: str) -> None:
@@ -6,16 +7,16 @@ class Database:
         self.key = key
         self.client: Client = create_client(url, key)
 
-    def get_student(self, student_id: str) -> dict:
+    def get_student(self, student_id: str) -> Student:
         return self.client.table('student_info').select('*').eq('student_id', student_id).execute().data[0]
     
     def delete_student(self, student_id: str) -> None:
         self.client.table('student_info').delete().eq('student_id', student_id).execute()
 
-    def update_student(self, student_id: str, data: dict) -> None:
+    def update_student(self, student_id: str, data: Student) -> Student:
         self.client.table('student_info').update(data).eq('student_id', student_id).execute()
 
-    def insert_student(self, data: dict) -> None:
+    def insert_student(self, data: Student) -> Student:
         self.client.table('student_info').insert(data).execute()
 
     def get_client(self) -> Client:
