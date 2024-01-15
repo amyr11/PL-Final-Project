10ccef29f5e5a84539fe65730fd1c594ce32d763
Dolliwyx <35372554+Dolliwyx@users.noreply.github.com>
2024-01-15 22:01:01 +0800

    docs: mOAR docs

--- common/types.py
+++ common/types.py
@@ -1,6 +1,18 @@
 from typing import TypedDict
 
 class Student(TypedDict):
+    """Student data type.
+
+    Attributes:
+        student_id (str): The student's ID.
+        first_name (str): The student's first name.
+        middle_name (str): The student's middle name.
+        last_name (str): The student's last name.
+        course (str): The student's course.
+        year_level (int): The student's year level.
+        email (str): The student's email.
+        contact_number (str): The student's contact number.
+    """
     student_id: str
     first_name: str
     middle_name: str
